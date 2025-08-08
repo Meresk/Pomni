5fefb4a4fead88de16888c668102c3b98b2cdb41
Яков Хватов <YakovX95@mail.ru>
2025-08-09 00:25:54 +0300

    Trimmer Converter for content in noteListImet added

---
+++ Pomni/Converters/NoteContentTrimmerConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Pomni.Converters
+{
+    public class NoteContentTrimmerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return string.Empty;
+
+            string text = value.ToString();
+            int maxLen = 55;
+
+            if (parameter != null && int.TryParse(parameter.ToString(), out int customMaxLen))
+                maxLen = customMaxLen;
+
+            return text.Length <= maxLen ? text : text.Substring(0, maxLen) + "...";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
